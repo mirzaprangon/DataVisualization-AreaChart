f592bd896bcc923c5ff9bd6c03dd409b03e43555
Mirza Prangon <38160503+mirzaprangon@users.noreply.github.com>
2018-07-23 19:08:52 +0600

    Added soruce files
    Uploaded source files from local.

---
+++ area.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+x1 = pd.ExcelFile("E:\\Data.xlsx")
+sheets = x1.sheet_names
+df = x1.parse(sheets[0])
+
+columns = list(df)
+c = columns[1:len(columns)]
+x = df[columns[0]].tolist()
+temp = []
+for col in c:
+    temp.append(np.array(df[col].tolist()))
+
+y = [(temp[0]), (temp[1]+temp[4]), (temp[1]+temp[4]+temp[3]), (temp[3])]
+lab = ['NRX', 'TRPs Contribution', 'Estimated TRPs', 'Base']
+
+for dat in range(4):
+    plt.plot(x, y[dat], label=lab[dat])
+
+for dat in y:
+    plt.fill_between(x,dat, alpha=.9)
+
+plt.legend(loc='upper left')
+plt.show()
---
+++ plot.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+x1 = pd.ExcelFile("E:\\Data.xlsx")
+sheets = x1.sheet_names
+df = x1.parse(sheets[0])
+
+columns = list(df)
+c = columns[1:len(columns)]
+x = df[columns[0]].tolist()
+temp = []
+
+for col in c:
+    temp.append(np.array(df[col].tolist()))
+
+y = [temp[0], temp[1]+temp[4], temp[1]+temp[4]+temp[3], temp[3]]
+lab = ['NRX', 'TRPs Contribution', 'Estimated TRPs', 'Base']
+
+plt.stackplot(x,y,labels=lab)
+plt.legend(loc='upper left')
+plt.yticks([7000, 10000, 13000, 16000, 19000])
+plt.show()
